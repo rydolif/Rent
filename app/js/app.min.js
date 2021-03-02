6a0ac6a55c99ae283bb0fedcb4c4f7af2338cf21
master <rudolifrudolif@gmail.com>
2021-03-02 21:48:38 +0200

    edits

--- app/js/app.min.js
+++ app/js/app.min.js
@@ -20,22 +20,6 @@ document.addEventListener("DOMContentLoaded", function() {
 	
 		};
 		headerFixed('.header', '.header--active');
-	
-	// //----------------------HAMBURGER-----------------------
-	// 	const hamburger = (hamburgerButton, hamburgerNav, hamburgerHeader) => {
-	// 		const button = document.querySelector(hamburgerButton),
-	// 					nav = document.querySelector(hamburgerNav),
-	// 					header = document.querySelector(hamburgerHeader);
-	
-	// 		button.addEventListener('click', (e) => {
-	// 			button.classList.toggle('hamburger--active');
-	// 			nav.classList.toggle('header__nav--active');
-	// 			header.classList.toggle('header--menu');
-	// 		});
-	
-	// 	};
-	// 	hamburger('.hamburger', '.header__nav', '.header');
-
 
 });
 	
